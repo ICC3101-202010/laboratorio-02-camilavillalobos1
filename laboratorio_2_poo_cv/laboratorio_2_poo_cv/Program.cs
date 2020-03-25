0df4db803160a152c95bc1245899924badd92c85
Camila Villalobos <camilavillalobos@MacBook-Air-de-Camila-6.local>
2020-03-25 15:14:17 -0300

    Agregando repositorio laboratorio 2

---
+++ laboratorio_2_poo_cv/laboratorio_2_poo_cv/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace laboratorio_2_poo_cv
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
